3adb0c608a09215413c6830b896a613eac77fcbb
Joaquin Rodriguez <rodriguezcjoaquin02@gmail.com>
2025-06-20 19:07:35 -0300

    add players

--- app/players.ts
+++ app/players.ts
@@ -3,9 +3,12 @@ export const players = [
   { id: "2", image: "/jero.png", x: 0, y: 0 },
   { id: "3", image: "/mati.png", x: 0, y: 0 },
   { id: "4", image: "/joaco.png", x: 0, y: 0 },
-  { id: "14", image: "/giuli.png", x: 0, y: 0 },
+  { id: "5", image: "/giuli.png", x: 0, y: 0 },
   { id: "6", image: "/agus.png", x: 0, y: 0 },
-  { id: "5", image: "/elias.png", x: 0, y: 0 },
+  { id: "7", image: "/elias.png", x: 0, y: 0 },
+  { id: "8", image: "/tima.png", x: 0, y: 0 },
+  { id: "9", image: "/pepa.png", x: 0, y: 0 },
+  { id: "10", image: "/emi.png", x: 0, y: 0 },
 ];
 export type Player = (typeof players)[number];
 export type Point = { x: number; y: number };
